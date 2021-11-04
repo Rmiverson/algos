2d22e4d3ab65197d6c1ec5f3bcab527d9995fbe2
Rmiverson <Rmiverson99@gmail.com>
2021-11-04 10:14:45 -0700

    wow, i guess i had the answer but just needed to make it the absolute value. im stupid

--- algorithms/DiagonalDifference.js
+++ algorithms/DiagonalDifference.js
@@ -9,34 +9,34 @@
 // answer is 2
 
 // expected: 15
-// let matrix = [
-//    [11, 2, 4],
-//    [4, 5, 6],
-//    [10, 8, -12]
-// ]
-
 let matrix = [
-   ["1", 2, 3],
+   [11, 2, 4],
    [4, 5, 6],
-   [9, 8, 9]
+   [10, 8, -12]
 ]
 
-function diff (arr) {
-   let rev = arr.map(a => {
-      return a.slice().reverse()
-   })   
+// let matrix = [
+//    ["1", 2, 3],
+//    [4, 5, 6],
+//    [9, 8, 9]
+// ]
 
-   const pek = (a) => {
+const diff = (arr) => {   
+   let reverse = arr.map(a => {
+      return a.slice().reverse()
+   })
+   
+   const getDiagonal = (array) => {
       let ans = []
-      for (let i = 0; i < a.length; i++) {
-         ans.push(parseInt(a[i][i]))
+      for (let i = 0; i < array.length; i++) {
+         ans.push(parseInt(array[i][i]))
       }
       return ans
    }
 
-   const reducer = (acc, currentVal) => acc + currentVal
+   const add = (acc, a) => acc + a
 
-   const ans =  pek(rev).reduce(reducer) - pek(arr).reduce(reducer)
+   const ans =  Math.abs(getDiagonal(reverse).reduce(add) - getDiagonal(arr).reduce(add))
    console.log(ans)
    return ans
 }
